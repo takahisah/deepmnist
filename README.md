2e326a42431614e034900850301cdaadd8d878ae
Taka Hasegawa <takahisah@gmail.com>
2017-05-13 21:00:02 -0700

    Add performance results for GPU machine

--- README.md
+++ README.md
@@ -8,3 +8,12 @@ A part of the TensorFlow introduction, implemented the code for [Deep MNIST for 
 
 ## Prerequisite
 - TensorFlow 1.0
+
+## Performance on GPU machine
+Accuracy on test set: 99.2%
+Time to run 20000 iterations: 130.848s
+
+## GPU machine specs
+- 16GB RAM
+- Intel core i5 kaby lake
+- NVIDIA GeForce GTX1060 6GB GPU
--- deepmnist.py
+++ deepmnist.py
@@ -1,5 +1,6 @@
 from tensorflow.examples.tutorials.mnist import input_data
 import tensorflow as tf
+import time
 
 mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
 sess = tf.InteractiveSession()
@@ -56,6 +57,8 @@ cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y_
 train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
 correct_prediction = tf.equal(tf.argmax(y_conv, 1), tf.argmax(y_, 1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+start = time.time()
 sess.run(tf.global_variables_initializer())
 for i in range(20000):
   batch = mnist.train.next_batch(50)
@@ -63,6 +66,9 @@ for i in range(20000):
     train_accuracy = accuracy.eval(feed_dict={x:batch[0], y_: batch[1], keep_prob: 1.0})
     print("step %d, training accuracy %g" % (i, train_accuracy))
   train_step.run(feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
+end = time.time()
+elapsed = end - start
 
+print("wall clock time: %g" % elapsed)
 print("test accuracy %g"%accuracy.eval(feed_dict={
     x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0}))
